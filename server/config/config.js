c104c52c15def318fd72a7549d52725d89036fb5
Omar-Mariot <omarariel.mariotpolonia@gmail.com>
2020-01-08 12:14:16 -0400

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,17 @@
 
 // Puerto
-
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Database
+let urlDB;
+
+if ( process.env.NODE_ENV === 'dev' ) {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://omariot:hKw6ZAVhEXsgQBFP@cafe-f6dvg.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
